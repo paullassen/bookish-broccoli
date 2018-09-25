265cce854f03c10de2dff150d3e38bd6741dde9a
Paul Lassen <Paul Lassen>
2018-09-25 19:20:56 +0400

    Did data analysis on Concrete Data and have begun plotting attributes against each other

--- DataPlot.py
+++ DataPlot.py
@@ -8,7 +8,8 @@ Created on Tue Sep 18 13:45:23 2018
 from DataExtraction import *
 
 from matplotlib.pyplot import (figure, subplot, plot, xlabel, ylabel, title, 
-yticks, show,legend,imshow, cm)
+yticks, show,legend,imshow, cm, hist)
+import matplotlib.pyplot as plt
 from scipy.io import loadmat
 import scipy.linalg as linalg
 import numpy as np
@@ -39,5 +40,30 @@ Z = Y @ V
 f=figure()
 ax = f.add_subplot(111, projection='3d')
 mask = y_bin==False;
-ax.plot(Z[y_bin,0],Z[y_bin,1],y[y_bin],'.')
-ax.plot(Z[mask,0],Z[mask,1],y[mask],'.')
+ax.plot(Z[y_bin,0],Z[y_bin,1],Z[y_bin,2],'.')
+ax.plot(Z[mask,0],Z[mask,1],Z[mask,2],'.')
+
+
+for i in range(0,8):
+    plt.subplot((241+i))
+    hist(X[X[:,i]>0,i])
+    ttl = attributeTitles[i]
+    title(ttl[:10])
+    plt.show()
+
+for i in range(0,8):
+    f = plt.figure()
+    for j in range(0,8):
+        ind = (j+1)
+        plt.subplot(2,4,ind)
+        plt.plot(X[:,i], X[:,j],'.')
+        ittl = attributeTitles[i]
+        jttl = attributeTitles[j]
+        xlabel(ittl[:6])
+        ylabel(jttl[:6])
+
+
+
+
+
+
